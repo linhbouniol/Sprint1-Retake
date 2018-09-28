b11e51e7ee6ef1f54056c7b2e5ff670622f3add9
Linh Bouniol <paulalinh89@gmail.com>
2018-09-28 09:21:58 -0700

    Passed movie controller to child VC in MovieTabBarController

---
+++ Sprint1-Retake/Protocols/MovieControllerProtocol.swift
@@ -0,0 +1,13 @@
+//
+//  MovieControllerProtocol.swift
+//  Sprint1-Retake
+//
+//  Created by Linh Bouniol on 9/28/18.
+//  Copyright © 2018 Linh Bouniol. All rights reserved.
+//
+
+import Foundation
+
+protocol MovieControllerProtocol: class {
+    var movieController: MovieController? { get set }
+}
--- Sprint1-Retake/View Controllers/MoviesTabBarController.swift
+++ Sprint1-Retake/View Controllers/MoviesTabBarController.swift
@@ -9,6 +9,12 @@
 import UIKit
 
 class MoviesTabBarController: UITabBarController {
+    
+    // MARK: - Property
+    
+    let movieController = MovieController()
+    
+    // MARK: - View Lifecycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -16,15 +22,13 @@ class MoviesTabBarController: UITabBarController {
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    // MARK: - Method
+    
+    func passMovieControllerToChildViewControllers() {
+        for childVC in children {
+            if let vc = childVC as? MovieControllerProtocol {
+                vc.movieController = movieController
+            }
+        }
     }
-    */
-
 }
