32996cb24555395c060a662a78c99b28e728872e
Linh Bouniol <paulalinh89@gmail.com>
2018-09-28 09:47:18 -0700

    Added movie list table view controller

--- Sprint1-Retake/View Controllers/AddMoviesViewController.swift
+++ Sprint1-Retake/View Controllers/AddMoviesViewController.swift
@@ -8,23 +8,21 @@
 
 import UIKit
 
-class AddMoviesViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+class AddMoviesViewController: UIViewController, MovieControllerProtocol {
+    
+    // MARK: - Property
+    
+    var movieController: MovieController?
+    
+    // MARK: - Outlets/Actions
+    
+    @IBOutlet weak var movieTextField: UITextField!
+    
+    @IBAction func addMovie(_ sender: Any) {
+        
+        guard let name = movieTextField.text, name.count > 0 else { return }
+        
+        movieController?.createMovie(withName: name)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
--- Sprint1-Retake/View Controllers/MovieListTableViewController.swift
+++ Sprint1-Retake/View Controllers/MovieListTableViewController.swift
@@ -8,23 +8,68 @@
 
 import UIKit
 
-class MovieListTableViewController: UIViewController {
+class MovieListTableViewController: UIViewController, MovieControllerProtocol, UITableViewDataSource, MovieListTableViewCellDelegate {
+    
+    // MARK: - Property
+    
+    var movieController: MovieController?
+    
+    // MARK: - Outlet
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    // MARK: - View Lifecycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    // MARK: - MovieListTableVieCellDelegate
+    
+    func isSeenButtonWasTapped(for cell: MovieListTableViewCell) {
+        
+        // need outlet for table view
+        guard let indexPath = tableView.indexPath(for: cell) else { return }
+        
+        guard let movie = movieController?.movies[indexPath.row] else { return }
+        
+        movieController?.toggleIsSeen(for: movie)
+        
+        // reload table view to show toggle, better to reload just the row that was modified then the entire table view
+        tableView.reloadRows(at: [indexPath], with: .automatic)
     }
-    */
-
+    
+    // MARK: - UITableViewDataSource
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return movieController?.movies.count ?? 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MovieCell", for: indexPath) as! MovieListTableViewCell
+        
+        guard let movie = movieController?.movies[indexPath.row] else { return cell }
+        
+        cell.movieLabel.text = movie.name
+        
+        // change the button label
+        if movie.isSeen {
+            cell.isSeenButton.setTitle("Seen", for: .normal)
+        } else {
+            cell.isSeenButton.setTitle("Not Seen", for: .normal)
+        }
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            guard let movie = movieController?.movies[indexPath.row] else { return }
+            
+            movieController?.delete(movie: movie)
+            tableView.deleteRows(at: [indexPath], with: .automatic)
+        }
+    }
 }
